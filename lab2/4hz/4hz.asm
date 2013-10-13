fb7d7d8c4cc103149d60154fadb8f40604152c3b
Austin Glaser <austin.glaser@gmail.com>
2013-10-12 19:21:46 -0600

    change addr to sfr name

--- lab2/4hz/4hz.asm
+++ lab2/4hz/4hz.asm
@@ -19,12 +19,12 @@
 
             ORG   $0000           ; Startup routine at 0000h
 
-T0_INIT     MOV   R2,#64H      ; count down from 100
-            MOV   R3,#09H      ; count down from 9 (*100 = 900)
-            MOV   TH0,#00H      ; init timer at 0
-            MOV   TL0,#00H      ; autoreload zero
-            MOV   TMOD,#02H      ; int clk,timer,8-bit autoreload (mode 2)
-            SETB  #8BH            ; 8Bh = TCON.4 = TR0. Turns on timer
+T0_INIT     MOV   R2,#64H         ; count down from 100
+            MOV   R3,#09H         ; count down from 9 (*100 = 900)
+            MOV   TH0,#00H        ; init timer at 0
+            MOV   TL0,#00H        ; autoreload zero
+            MOV   TMOD,#02H       ; int clk,timer,8-bit autoreload (mode 2)
+            SETB  TCON.4          ; 8Bh = TCON.4 = TR0. Turns on timer
 WAIT        SJMP  WAIT
 
 
@@ -40,10 +40,10 @@ WAIT        SJMP  WAIT
             ORG   $000B           ; timer zero interrupt vector is 000B.
 
 ISR_T0      SETB  P1.2            ; signal interrupt begin
-            DJNZ  R2,INT_DONE  ; count down from 100
-            MOV   R2,#64H      ; re-init r2
-            DJNZ  R3,INT_DONE  ; count down from 9 (*100)
-            MOV   R2,#09H      ; re-init r3
+            DJNZ  R2,INT_DONE     ; count down from 100
+            MOV   R2,#64H         ; re-init r2
+            DJNZ  R3,INT_DONE     ; count down from 9 (*100)
+            MOV   R2,#09H         ; re-init r3
             CPL   P1.1            ; toggle LED pin
 INT_DONE    CLR   P1.2            ; signal interrupt end
             RETI                  ; continue waiting in main routine
