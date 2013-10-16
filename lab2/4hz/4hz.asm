5efc08033470307e0dcc7a3a4463851ce7290892
Austin Glaser <austin.glaser@gmail.com>
2013-10-15 20:24:23 -0600

    modify timer int routine to use 16 bit timer

--- lab2/4hz/4hz.asm
+++ lab2/4hz/4hz.asm
@@ -23,16 +23,16 @@ STARTUP     AJMP  T0_INIT         ; go to init routine (need to make space for I
 
             ORG   $0100
 
-T0_INIT     MOV   R2,#64H         ; count down from 100
-            MOV   R3,#09H         ; count down from 9 (*100 = 900)
-            MOV   TH0,#00H        ; init timer at 0
-            MOV   TL0,#00H        ; autoreload zero
-            MOV   TMOD,#02H       ; int clk,timer,8-bit autoreload (mode 2)
-            SETB  TCON.4          ; TCON.4 = TR0. Turns on timer
-            SETB  IE.7            ; IE.7 = EA. Enables interrupts
+T0_INIT     MOV   R2,#04H         ; count down from 4
+            MOV   TH0,#E1H        ; init timer at E104h
+            MOV   TL0,#04H
+            MOV   TMOD,#01H       ; int clk,timer,16-bit (mode 1)
             SETB  IE.1            ; IE.1 = ET0. Enables timer 0 interrupt
-WAIT        CPL   P1.3            ; indicate we're in loop
-            SJMP  WAIT            ; idle loop
+            SETB  IE.7            ; IE.7 = EA. Enables interrupts
+            SETB  TCON.4          ; TCON.4 = TR0. Turns on timer
+WAIT        NOP                   ; idle loop
+            CPL   P1.3
+            SJMP  WAIT
 
 
 ; Interrupt. Generated with frequency 3600 Hz so
@@ -48,10 +48,11 @@ WAIT        CPL   P1.3            ; indicate we're in loop
             ORG   $000B           ; timer zero interrupt vector is 000B.
 
 ISR_T0      SETB  P1.2            ; signal interrupt begin
-            DJNZ  R2,INT_DONE     ; count down from 100
-            MOV   R2,#64H         ; re-init r2
-            DJNZ  R3,INT_DONE     ; count down from 9 (*100)
-            MOV   R2,#09H         ; re-init r3
+            MOV   TH0,#E1H        ; init timer at 0
+            MOV   TL0,#04H        ; autoreload zero
+            SETB  TCON.4          ; TCON.4 = TR0. Turns on timer
+            DJNZ  R2,INT_DONE     ; count down from 4
+            MOV   R2,#04H         ; re-init r2
             CPL   P1.1            ; toggle LED pin
 INT_DONE    CLR   P1.2            ; signal interrupt end
             RETI                  ; continue waiting in main routine
