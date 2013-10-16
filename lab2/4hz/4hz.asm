70e20e06433beb50ac917bda952415edf0aea4c1
Austin Glaser <austin.glaser@gmail.com>
2013-10-15 20:45:01 -0600

    change timer init value

--- lab2/4hz/4hz.asm
+++ lab2/4hz/4hz.asm
@@ -9,13 +9,11 @@
 
 ; Initialization routine. Sets up timer0 and registers
 ; for 4 Hz signal generation.
-; Internal oscillator: 11.0592 MHz
 ; Internal Clock: osc/12 = 921.6 kHz
-; 8 bit autoreload counts to 256,generating interrupts
-; with frequency 3.6 kHz.
-; To get 4 Hz,we must switch every 900 interrupts
-; by decrementing two registers (initialized to 9 and 100
-; respectively)
+; 16 bit counter, initialized to E104h, interrupts with
+; frequence ~ 16 Hz
+; To get 4 Hz,we must switch every 4 interrupts
+; by decrementing a register
 
             ORG   $0000           ; Startup routine at 0000h
 
@@ -24,8 +22,8 @@ STARTUP     AJMP  T0_INIT         ; go to init routine (need to make space for I
             ORG   $0100
 
 T0_INIT     MOV   R2,#04H         ; count down from 4
-            MOV   TH0,#e1H        ; init timer at E104h
-            MOV   TL0,#04H
+            MOV   TH0,#1EH        ; init timer at 1EFCh
+            MOV   TL0,#FCH
             MOV   TMOD,#01H       ; int clk,timer,16-bit (mode 1)
             SETB  IE.1            ; IE.1 = ET0. Enables timer 0 interrupt
             SETB  IE.7            ; IE.7 = EA. Enables interrupts
@@ -35,9 +33,9 @@ WAIT        NOP                   ; idle loop
             SJMP  WAIT
 
 
-; Interrupt. Generated with frequency 3600 Hz so
+; Interrupt. Generated with frequency 16 Hz so
 ; routine counts number of calls and switches every
-; 900.
+; 4.
 ;
 ; NOTE: This routine can ONLY be used as-is if
 ; other interrupts are not used. If they are,
@@ -48,8 +46,8 @@ WAIT        NOP                   ; idle loop
             ORG   $000B           ; timer zero interrupt vector is 000B.
 
 ISR_T0      SETB  P1.2            ; signal interrupt begin
-            MOV   TH0,#e1H        ; init timer at 0
-            MOV   TL0,#04H        ; autoreload zero
+            MOV   TH0,#1EH        ; init timer at 1EFCh
+            MOV   TL0,#FCH
             SETB  TCON.4          ; TCON.4 = TR0. Turns on timer
             DJNZ  R2,INT_DONE     ; count down from 4
             MOV   R2,#04H         ; re-init r2
