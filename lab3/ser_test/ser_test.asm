d3f5d4b9850d7229ad743a259364dcdbe22bb4cc
Austin Glaser <austin.glaser@gmail.com>
2013-10-28 22:51:10 -0600

    add delay between chars; skeleton for echo

--- lab3/ser_test/ser_test.asm
+++ lab3/ser_test/ser_test.asm
@@ -20,8 +20,11 @@ STARTUP     MOV   SCON,#50H       ; Mode 1: 8 bit UART. REN = 1
 
 TRANSMIT    MOV   SBUF,#'U'       ; Send the letter 'U'
 
-            MOV   R2,#FF          ; add some space between characters
-DELAY       DJNZ  R2,DELAY
+            MOV   R3,#FF          
+DELAY_OUT   MOV   R2,#FF          ; add some space between characters
+DELAY_IN    DJNZ  R2,DELAY_IN
+            DJNZ  R3,DELAY_OUT
+
 
 WAIT        JNB   SCON.1,WAIT     ; Wait till the last char is sent (SCON.1 = TI)
             CLR   SCON.1          ; Tell hardware to send next char
