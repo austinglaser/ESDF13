7166fe8337cc20acd90a502202ba0ec05b23ff36
Austin Glaser <austin.glaser@gmail.com>
2013-10-28 22:24:44 -0600

    change transmit to a

--- lab3/ser_test/ser_test.asm
+++ lab3/ser_test/ser_test.asm
@@ -18,7 +18,7 @@ STARTUP     MOV   SCON,#50H       ; Mode 1: 8 bit UART. REN = 1
             MOV   TMOD,#20H       ; timer 1: 8 bit autoreload (mode 2)
             SETB  TCON.6          ; Enable timer 1
 
-TRANSMIT    MOV   SBUF,#'U'       ; Send the letter 'U'
+TRANSMIT    MOV   SBUF,#'A'       ; Send the letter 'U'
 
 
 WAIT        JNB   SCON.1,WAIT     ; Wait till the last char is sent (SCON.1 = TI)
