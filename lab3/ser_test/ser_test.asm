4b903a0c2a7f8fe71f0d1b9f51da09be1b997b1c
Austin Glaser <austin.glaser@gmail.com>
2013-10-28 21:46:32 -0600

    Turn off baud doubling, enable recieve

--- lab3/ser_test/ser_test.asm
+++ lab3/ser_test/ser_test.asm
@@ -3,12 +3,14 @@
 ;
 ; Continuously transmits the letter 'U' on the serial port.
 ; Baud rate set to 9600
+;
+; Based off code found at http://www.freewebs.com/maheshwankhede/rs232.html
 
             ORG   $0000
 
-STARTUP     MOV   SCON,#40H       ; Mode 1: 8 bit UART. No receive right now.
-            ORL   PCON,#70H       ; Use baud rate doubling (set PCON.7)
-            MOV   TH1,#0FAH       ; Baud rate: 9600
+STARTUP     MOV   SCON,#50H       ; Mode 1: 8 bit UART. REN = 1
+            ANL   PCON,#07FH      ; turn off baud rate doubling (clear PCON.7)
+            MOV   TH1,#0FDH       ; Baud rate: 9600
             MOV   TMOD,#20H       ; timer 1: 8 bit autoreload (mode 2)
             SETB  TCON.6          ; Enable timer 1
 
@@ -16,5 +18,5 @@ TRANSMIT    MOV   SBUF,#'U'       ; Send the letter 'U'
 
 
 WAIT        JNB   SCON.1,WAIT     ; Wait till the last char is sent (SCON.1 = TI)
-            CLR   SCON.1          ; Tell software to send next char
-            AJMP  TRANSMIT        ; infinite loop
+            CLR   SCON.1          ; Tell hardware to send next char
+            SJMP  TRANSMIT        ; infinite loop
