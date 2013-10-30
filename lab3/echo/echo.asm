d41e49da802ed705aa73e7921d18811b0dfc828a
Austin Glaser <austin.glaser@gmail.com>
2013-10-30 13:06:02 -0600

    change TI/RI clear order

--- lab3/echo/echo.asm
+++ lab3/echo/echo.asm
@@ -18,8 +18,8 @@ STARTUP     MOV   SCON,#50H       ; mode 1: 8 bit UART. REN = 1
             ANL   SCON,#0FCH       ; Clear TI and RI
 
 WAITR       JNB   SCON.0,WAITR    ; wait till character is recieved
-            CLR   SCON.0          ; clear RI
             CLR   SCON.1          ; immediately send what's in the buffer
+            CLR   SCON.0          ; clear RI
 
 WAITT       JNB   SCON.1,WAITT    ; Wait for byte to be transmitted            
             SJMP  WAITR           ; continue waiting for recieve
