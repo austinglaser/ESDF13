c2a740ad5776fb607fcb3bfa53b4ada78bfe6ae4
Austin Glaser <austin.glaser@gmail.com>
2013-10-30 16:03:29 -0600

    add serial capability to memtest

--- lab3/memtest/memtest.asm
+++ lab3/memtest/memtest.asm
@@ -13,7 +13,8 @@ STARTUP   AJMP  MAIN
 
           ORG   $0100
 
-MAIN      MOV   R2,#02H         ; count down from 2
+MAIN      
+T0_INIT   MOV   R2,#02H         ; count down from 2
           MOV   TH0,#1EH        ; init timer at 1EFCh
           MOV   TL0,#0FCH
           MOV   TMOD,#01H       ; int clk,timer,16-bit (mode 1)
@@ -21,22 +22,44 @@ MAIN      MOV   R2,#02H         ; count down from 2
           SETB  IE.7            ; IE.7 = EA. Enables interrupts
           SETB  TCON.4          ; TCON.4 = TR0. Turns on timerV
 
+SER_INIT  MOV   SCON,#50H       ; mode 1: 8 bit UART. REN = 1
+          ORL   PCON,#80H       ; Double baud rate
+          MOV   TH1,#0FDH       ; Baud rate: 19200
+          MOV   TMOD,#20H       ; timer 1: 8 bit autoreload (mode 2)
+          SETB  TCON.6          ; Enable timer 1
+          ANL   SCON,#0FCH      ; Clear TI and RI
+
 TEST_BOT  MOV   DPTR,#0400h     ; Test bottom of memory
           ACALL TEST_MEM
-          CJNE  A,#00h,SPIN     ; simply end on failure
+          CJNE  A,#00h,FAIL_BOT ; go to failure
+          AJMP  TEST_TOP        ; move on to nest test
+
+FAIL_BOT  MOV   A,#'B'          ; send 'B' to indicate where failed
+          ACALL SEND_CHAR
+          AJMP  SPIN            ; keep spinning
 
 TEST_TOP  MOV   DPTR,#07FFFh    ; Test top
           ACALL TEST_MEM
-          CJNE  A,#00h,SPIN     ; simply end on failure
+          CJNE  A,#00h,FAIL_TOP ; go to failure
+          AJMP  TEST_TOP        ; move on to nest test
+
+FAIL_TOP  MOV   A,#'T'          ; send 'B' to indicate where failed
+          ACALL SEND_CHAR
+          AJMP  SPIN            ; keep spinning
 
 TEST_MID  MOV   DPTR,#0400h     ; Test middle of memory
           ACALL TEST_MEM
-          CJNE  A,#00h,SPIN     ; simply end on failure
+          CJNE  A,#00h,FAIL_BOT ; go to failure
+          AJMP  TEST_TOP        ; move on to nest test
 
-LED_ON    CLR   TCON.4          ; turn LED on when successful
+FAIL_MID  MOV   A,#'M'          ; send 'B' to indicate where failed
+          ACALL SEND_CHAR
+          AJMP  SPIN            ; keep spinning
+
+LED_ON    CLR   TCON.4          ; turn off blinking when successful
           CLR   IE.7
           CLR   IE.1
-          CLR   P1.1
+          CLR   P1.1            ; turn on LED
 
 SPIN      SJMP  SPIN            ; spin when all done
 
@@ -67,6 +90,14 @@ SUCCESS   MOV   A,#00h          ; It worked!
 FAIL      MOV   A,#01h          ; it didn't work...
 
 END       RET
+
+; sends the character in the accumulator on serial
+
+SEND_CHAR JNB   SCON.1,SEND_CHAR ; wait for bit to finish sending
+          MOV   SBUF,A          ; put it in the buffer
+          CLR   SCON.1          ; Indicate send
+
+          RET
 
 ; Interrupt. Generated with frequency 16 Hz so
 ; routine counts number of calls and switches every
