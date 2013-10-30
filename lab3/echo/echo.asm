3dcb2cf3c32efd1dd1d8b0ac93c81c8495dfbe2a
Austin Glaser <austin.glaser@gmail.com>
2013-10-30 13:08:15 -0600

    clear RI after send

--- lab3/echo/echo.asm
+++ lab3/echo/echo.asm
@@ -19,7 +19,7 @@ STARTUP     MOV   SCON,#50H       ; mode 1: 8 bit UART. REN = 1
 
 WAITR       JNB   SCON.0,WAITR    ; wait till character is recieved
             CLR   SCON.1          ; immediately send what's in the buffer
-            CLR   SCON.0          ; clear RI
 
 WAITT       JNB   SCON.1,WAITT    ; Wait for byte to be transmitted            
+            CLR   SCON.0          ; clear RI
             SJMP  WAITR           ; continue waiting for recieve
