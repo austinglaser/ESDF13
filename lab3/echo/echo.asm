49de49858dced49a94b355481de30e4035f36c74
Austin Glaser <austin.glaser@gmail.com>
2013-10-30 12:48:48 -0600

    add clear statements for TI and RI

--- lab3/echo/echo.asm
+++ lab3/echo/echo.asm
@@ -15,8 +15,10 @@ STARTUP     MOV   SCON,#50H       ; mode 1: 8 bit UART. REN = 1
             MOV   TH1,#0FDH       ; Baud rate: 19200
             MOV   TMOD,#20H       ; timer 1: 8 bit autoreload (mode 2)
             SETB  TCON.6          ; Enable timer 1
+            ANL   SCON,#FCH       ; Clear TI and RI
 
 WAITR       JNB   SCON.0,WAITR    ; wait till character is recieved
+            CLR   SCON.0          ; clear RI
             CLR   SCON.1          ; immediately send what's in the buffer
 
 WAITT       JNB   SCON.1,WAITT    ; Wait for byte to be transmitted            
