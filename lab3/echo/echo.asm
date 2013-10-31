c70dafd75230c3f542420db2dd95cc002cc581a7
Austin Glaser <austin.glaser@gmail.com>
2013-10-30 18:22:46 -0600

    echo again

--- lab3/echo/echo.asm
+++ lab3/echo/echo.asm
@@ -15,7 +15,8 @@ STARTUP     MOV   SCON,#50H       ; mode 1: 8 bit UART. REN = 1
             MOV   TH1,#0FDH       ; Baud rate: 9600
             MOV   TMOD,#20H       ; timer 1: 8 bit autoreload (mode 2)
             SETB  TCON.6          ; Enable timer 1
-            ANL   SCON,#0FCH      ; Clear TI and RI
+            CLR   SCON.1
+            CLR   SCON.0
 
 WAITR       JNB   SCON.0,WAITR    ; wait till character is recieved
             CLR   SCON.0          ; clear RI; acknowledge receipt
