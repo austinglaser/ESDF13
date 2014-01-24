2f642771bb35c5d9d5cc609a9c97d81adafe9347
Austin Glaser <austin.glaser@gmail.com>
2014-01-23 19:32:00 -0700

    string reading function

--- lab3/lab3_final/src/main.c
+++ lab3/lab3_final/src/main.c
@@ -6,11 +6,24 @@ void setup(void);
 
 int main(void)
 {
+  char c;
+  char buffer[128];
+  int i;
   printf("Hi, welcome!\n");
-  printf("Type a 'c': ");
-  while(getchar() != 'c');
 
-  printf("\nYou hit the right key!");
+  printf("Enter a string (truncates after 128 chars): ");
+
+
+  for (i = 0; i < 127; i++) {
+    c = getchar();
+    if (c == '\n') break;
+    buffer[i] = c;
+  }
+  buffer[i] = '\0';
+
+
+  printf("You entered %s!\n", buffer);
+
 
   while(1);
 }
