78e1415935aa0d695ff521afa0b7086661c1d0b1
Austin Glaser <austin.glaser@gmail.com>
2014-01-25 14:44:45 -0700

    add assembly rules to makefile, write debugport in assembly

--- lab3/lab3_final/src/debugport.c
+++ lab3/lab3_final/src/debugport.c
@@ -1,8 +1,37 @@
 #include "debugport.h"
 
 // simply writes a value to the specified address.
-// Could be made more efficient with assembly
 void dataout(char xdata * address, char val) 
 {
-  *address = val;
+  __asm
+    ;'address' initially stored in dptr
+    ;'val' stored at #_dataout_PARM_2 in xdata
+    
+    ;*address = val;
+
+    ; save address value
+    mov   r2, dpl
+    mov   r3, dph
+
+    ; load val from xdata
+    mov   dptr,#_dataout_PARM_2
+    movx  a, @dptr
+
+    ; restore dptr
+    mov   dpl, r2
+    mov   dph, r3
+
+    ; write to the desired address
+    movx  @dptr, a
+  __endasm;
+}
+
+// writes val to the fixed address 0xFFFF
+void debugout(char val)
+{
+  __asm
+    mov   a, dpl          ; val stored in dpl initially
+    mov   dptr, #0xFFFF   ; load in fixed debugport address
+    movx  @dptr, a 
+  __endasm;
 }
--- lab3/lab3_final/src/debugport.h
+++ lab3/lab3_final/src/debugport.h
@@ -4,12 +4,15 @@
 // Writes 'x' to an unused address (FFFFh).
 // Only functional when DEBUG defined
 #ifdef DEBUG
-#define DEBUGPORT(x) dataout(0xFFFF, (x))
+#define DEBUGPORT(x) debugout(x)
 #else
 #define DEBUGPORT(x)
 #endif
 
 // Writes the value to the specified xdata address
 void dataout(char xdata * address, char  val);
+
+// writes val to the fixed address 0xFFFF
+void debugout(char val);
 
 #endif
